80d0478999319ec7cfe0c32cdc50785a27209e57
iSaacSigei <kiplangatisack.is@gmail.com>
2022-12-04 17:46:46 +0300

    generated the corresponding routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :hero_powers, only: [:create]
+  resources :powers, only: [:show, :index,:update]
+  get '/heroes', to: "heroes#index"
+  get '/heroes/:id', to: "heroes#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
